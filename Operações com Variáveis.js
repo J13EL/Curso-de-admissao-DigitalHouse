6ae202bc0dea403758b88c8e2de93496aa95e66c
J13EL <81261343+J13EL@users.noreply.github.com>
2021-12-22 16:03:21 -0300

    Módulo 2 - Aula Virtual 01
    Vamos ver se você entendeu: Declare duas variáveis ​​numeroA e numeroB e atribua valores numéricos à elas. Depois, crie as seguintes variáveis:
    - resultadoSoma - contendo a soma de numeroA e numeroB
    - resultadoMultiplicacao - contendo a multiplicação de numeroA e numeroB
    - resultadoDivisao - contendo a divisão de numeroA e numeroB
    - resultadoSubtracao - contendo a subtração de numeroA e numeroB

---
+++ Operações com Variáveis.js
@@ -0,0 +1,7 @@
+let numeroA = 10
+let numeroB = 15
+let outroNumero = numeroA + numeroB
+let resultadoSoma = outroNumero
+let resultadoSubtracao = numeroA - numeroB
+let resultadoDivisao = numeroA / numeroB
+let resultadoMultiplicacao = numeroA * numeroB
